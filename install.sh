969a68b4d7006cd56b376a88042168205554560f
chenkete <chenkete@yeah.net>
2019-11-24 12:54:35 +0800

    installation script improvement

--- install.sh
+++ install.sh
@@ -2,9 +2,37 @@
 
 set -e
 
+SCRIPT_NAME=$0
+NOVUNDLE=0
+
+usage() {
+cat << EOF
+usage: $SCRIPT_NAME [-novundle]
+	-novundle: no git operation on vundle(for faster test)
+EOF
+	exit 1
+}
+
+while [ -n "$1" ]; do
+	case "$1" in
+		-novundle)
+			NOVUNDLE=1;;
+		--help | -h)
+			usage;;
+		*)
+			echo "invalid arg: $1"
+			usage
+	esac
+	shift
+done
+
 print_and_exit() {
 	echo "$*"
 	exit 1
+}
+
+my_log() {
+	printf "\033[32m%s\033[0m\n" "$*"
 }
 
 OS=`uname`
@@ -15,13 +43,21 @@ fi
 VIM_CONF_DIR=$HOME/.vim
 VIMRC=$HOME/.vimrc
 
-echo "clone Vundle"
-git clone https://github.com/VundleVim/Vundle.vim.git $VIM_CONF_DIR/bundle/Vundle.vim
+if [ $NOVUNDLE -eq 0 ]; then
+	VUNDLE=$VIM_CONF_DIR/bundle/Vundle.vim
+	if [ -d $VUNDLE ]; then
+		my_log "$VUNDLE exists, git pull"
+		$SHELL -c "cd $VUNDLE && git pull"
+	else
+		my_log "clone Vundle"
+		git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE
+	fi
+fi
 
-echo "copy .vimrc"
+my_log "copy .vimrc"
 cp vimrc $VIMRC
  
-echo "install plugin"
+my_log "install plugin"
 vim -c "PluginInstall" -c "qa!"
 
 
